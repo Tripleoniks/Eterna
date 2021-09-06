b68e3e0542c2d27d1089deed16cad5dee000a1b6
LiamNussie <65913823+LiamNussie@users.noreply.github.com>
2021-09-06 22:23:52 +0100

    p

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -5,10 +5,11 @@ const Dashboard = () => {
     return ( 
         <div className="dashboard">
             <h1>DASHBOARD PAGE</h1>
-            {chartDatax.map(({title}, index) => {
+            {chartDatax.map(({title, id}) => {
                 return (
-                    <div className="item" key={index}>
+                    <div className="item" key={id}>
                        <p className="title">{title}</p>
+
                     </div>
                 )
             })}
